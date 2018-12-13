12a7dade8c729de6fed06c331af7e41ca5a9c890
IsolationWyn <genius_wz@aliyun.com>
2018-12-13 21:26:40 +0800

    [feat] change current root file system

--- container/init.go
+++ container/init.go
@@ -18,8 +18,8 @@ func RunContainerInitProcess() error {
 	/*
 	使用mount去挂载proc文件系统, 以便后面通过ps等命令去查看当前进程资源的情况
 	init进程读取了父进程传递过来的参数后, 在子进程内进行了执行, 这样就完成了将用户指定命令传递给子进程的操作
-
 	*/
+	
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 
@@ -48,3 +48,59 @@ func readUserCommand() []string {
 }
 
 
+func pivotRoot(root string) error {
+	/**
+	为了使当前root和老root不在同一文件系统下, 重新mount root
+	bind mount 是把相同的内容换了一个挂载点的方法
+	*/
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		return fmt.Errorf("Mount rootfs to itself error: %v", err)
+	}
+
+	// 创建 rootfs/.pivot_root 存储 old_root
+	pivotRoot := filepath.Join(root, ".pivot_root")
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
+		return err
+	}
+
+	// pivot_root 到新的rootfs, 现在老的 old_root 是挂载在rootfs/.pivot_root
+	// 挂载点现在依然可以在mount命令中看到
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("pivot_root %v", err)
+	}
+
+	// 修改当前的工作目录到根目录
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir / %v", err)
+	}
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+	// umount rootfs/.pivot
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("unmount pivot_root dir %var", err)
+	}
+
+	// 删除临时文件夹
+	return os.Remove(pivotDir)
+}
+
+
+/*
+init 挂载点
+*/
+func setupMount() {
+	// 获取当前路径
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Errorf("Get current location error %v", err)
+		return 
+	}
+	log.Infof("Current location is %s", pwd)
+	pivotRoot(pwd)
+
+	// mount proc
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+}
