1b739e5da7007a0538d494474c85b48bd5d375d8
IsolationWyn <zi.wang96@outlook.com>
2019-04-24 10:03:29 +0800

    [fix] pivotRoot

--- container/init.go
+++ container/init.go
@@ -49,73 +49,53 @@ func readUserCommand() []string {
 	return strings.Split(msgStr, " ")
 }
 
+/**
+Init 挂载点
+*/
+func setUpMount() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Errorf("Get current location error %v", err)
+		return
+	}
+	log.Infof("Current location is %s", pwd)
+	pivotRoot(pwd)
+
+	//mount proc
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+}
+
 func pivotRoot(root string) error {
 	/**
-	为了使当前root和老root不在同一文件系统下, 重新mount root
-	bind mount 是把相同的内容换了一个挂载点的方法
+	  为了使当前root的老 root 和新 root 不在同一个文件系统下，我们把root重新mount了一次
+	  bind mount是把相同的内容换了一个挂载点的挂载方法
 	*/
-	// 执行bind挂载, 使文件或者子目录在文件系统内的另一个点上可视 | 
 	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
 		return fmt.Errorf("Mount rootfs to itself error: %v", err)
 	}
-
-	// 创建 rootfs/.pivot_root 存储 old_root 	
+	// 创建 rootfs/.pivot_root 存储 old_root
 	pivotDir := filepath.Join(root, ".pivot_root")
-
 	if err := os.Mkdir(pivotDir, 0777); err != nil {
 		return err
 	}
-
 	// pivot_root 到新的rootfs, 现在老的 old_root 是挂载在rootfs/.pivot_root
 	// 挂载点现在依然可以在mount命令中看到
 	if err := syscall.PivotRoot(root, pivotDir); err != nil {
 		return fmt.Errorf("pivot_root %v", err)
 	}
-
 	// 修改当前的工作目录到根目录
 	if err := syscall.Chdir("/"); err != nil {
 		return fmt.Errorf("chdir / %v", err)
 	}
 
 	pivotDir = filepath.Join("/", ".pivot_root")
-	// umount rootfs/.pivot
+	// umount rootfs/.pivot_root
 	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
 		return fmt.Errorf("unmount pivot_root dir %v", err)
 	}
-
 	// 删除临时文件夹
 	return os.Remove(pivotDir)
-}
-
-
-/*
-init 挂载点
-*/
-func setupMount() {
-	// 获取当前路径
-	pwd, err := os.Getwd()
-	if err != nil {
-		log.Errorf("Get current location error %v", err)
-		return 
-	}
-	log.Infof("Current location is %s", pwd)
-	if err := pivotRoot(pwd); err != nil {
-		log.Error("fail to pivot_root ", err)
-	}
-
-	// mount proc
-	// 不允许在挂载的文件系统上执行程序 | 执行程序时，不遵照set-user-ID 和 set-group-ID位 | 不允许访问设备文件
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	err = syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
-	if err != nil {
-		log.Error("Mount proc error %v", err)
-	}
-
-	// 执行程序时，不遵照set-user-ID 和 set-group-ID位 | 总是更新最后访问时间
-	// tmpfs 是一种基于内存的文件系统, 可以使用RAM或swap分区来存储\
-	
-	err = syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
-	if err != nil {
-		log.Error("Mount proc error %v", err)
-	}
 }
